4abca68ca65b617ea9fb63c21444769fec1661e5
henriquecuryboaro <henriquecuryboaro>
2026-02-11 22:44:21 -0300

    alterações no script do aplicativo

--- app.py
+++ app.py
@@ -48,12 +48,22 @@ def variavel_agreg_periodo(empresa,variavel,inicio,fim):
 
     return round(filtro_temporal.sum(skipna=True),2)
 
+@st.cache_data
+def variavel_media(empresa,variavel,inicio,fim):
+    inicio = pd.to_datetime(inicio)
+    fim = pd.to_datetime(fim)
+    filtro_temporal = data[(data['facility_name'] == empresa) & (data['year_month'] >= inicio) & (data['year_month'] <= fim)]
+    filtro_temporal = filtro_temporal[variavel]
+
+    return round(filtro_temporal.mean(skipna=True),2)
+
+
 def main():
 
-    st.write('# Painel de indicadores operacionais de indústrias químicas')
+    st.write('# Indicadores operacionais em plantas industriais')
     st.sidebar.title('Menu de navegação')
     inicio = st.sidebar.date_input('### Início da série', min_value=date(2010, 1, 1), max_value=None)
-    fim = st.sidebar.date_input('### Fim da série', min_value=date(2026, 12, 31), max_value=None)
+    fim = st.sidebar.date_input('### Fim da série', min_value=date(2010, 1, 1), max_value=None)
     facility_escolhida = st.sidebar.selectbox('Escolha a planta',sorted(facilities_list), index=None, placeholder='Plantas', key=f'planta')
 
     col1,col2 = st.columns(2)
@@ -64,6 +74,7 @@ def main():
         kpi_escolhida = st.selectbox('Escolha o KPI',sorted(kpi_lista), index=None, placeholder='Indicadores', key=f'indicador')
 
         try:
+          
             data_facility = data[data['facility_name'] == facility_escolhida]
             fig = px.scatter(
                 data_facility,
@@ -74,15 +85,43 @@ def main():
             )
 
             st.plotly_chart(fig)
+
         except:
             pass
 
+
+
     with con2:
-        variavel_somar = st.selectbox('Esolha indicador',sorted(indicadores_para_somatorios),index=None,placeholder='Indicadores',key='indicador_soma')
-        
+       
         try:
-            soma_obtida = variavel_agreg_periodo(facility_escolhida,variavel_somar,inicio,fim)
-            st.metric(label=f'Somatório de {variavel_somar}', value=soma_obtida, border=True)
+            col1, col2 = st.columns(2)
+            somatorio_lucro = variavel_agreg_periodo(facility_escolhida,'gross_profit',inicio,fim)
+            somatorio_ebitda = variavel_agreg_periodo(facility_escolhida,'ebitda',inicio,fim)
+            somatorio_lucro_liquido = variavel_agreg_periodo(facility_escolhida,'net_income',inicio,fim)
+            somatorio_receita = variavel_agreg_periodo(facility_escolhida,'revenue',inicio,fim)
+
+            oee_medio = variavel_media(facility_escolhida,'oee_pct',inicio,fim)
+            margem_ebitda_media = variavel_media(facility_escolhida,'ebitda_margin_pct',inicio,fim)
+
+            fig_oee = go.Figure(go.Indicator(
+                    mode = "gauge+number",
+                    value = oee_medio,
+                    gauge = {"axis": {"range":[0,100]}},
+                    title = {'text': "OEE Médio (%)"}))
+            
+            fig_margem_ebitda = go.Figure(go.Indicator(
+                    mode = "gauge+number",
+                    value = margem_ebitda_media,
+                    gauge = {"axis": {"range":[0,100]}},
+                    title = {'text': "Margem EBITDA(%)"}))
+
+            col1.metric(label=f'Lucro bruto (em milhares)', value=f'US$ {somatorio_lucro}', border=True)
+            col1.metric(label=f'Receita (em milhares)', value=f'USS {somatorio_receita}', border=True)
+            col1.plotly_chart(fig_oee, use_container_width=True)
+            col2.metric(label=f'Lucro líquido (em milhares', value=f'US$ {somatorio_lucro_liquido}', border=True)
+            col2.metric(label=f'EBITDA (em milhares)', value=f'US$ {somatorio_ebitda}', border=True)
+            col2.plotly_chart(fig_margem_ebitda, use_container_width=True)
+
         except:
             pass
 
